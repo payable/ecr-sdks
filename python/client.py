712894ef21ac14af702eba857179877194dc2eed
Aslam Anver <slam.anver9@gmail.com>
2020-09-30 10:23:07 +0530

    Changes

--- python/client.py
+++ python/client.py
@@ -1,10 +1,8 @@
 import websocket
 
 ws = websocket.WebSocket()
-ws.connect('ws://192.168.2.222:45454')
-print('connected')
+ws.connect('ws://192.168.2.204:45454')
+ws.send('{"amount":20,"endpoint":"PAYMENT","method":"CARD","order_tracking":"example_sale_from_test","receipt_email":"customer@gmail.com","receipt_sms":"0777777777","id":1}')
 while(True):
     result = ws.recv()
     print(result)
-    # ws.send('from Python: ' + result)
-    ws.send('{"amount":200,"id":1,"method":"CARD","order_tracking":"666"}')
