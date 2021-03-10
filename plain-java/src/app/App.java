9e207ff0df7eac054c4462e72a5fca1355c2695f
Aslam Anver <slam.anver9@gmail.com>
2021-03-10 16:55:48 +0530

    ecr client and vb6 developments

--- plain-java/src/app/App.java
+++
@@ -1,8 +0,0 @@
-package app;
-
-public class App {
-
-    public static void main(String[] args) {
-        new UIDemo().start();
-    }
-}
--- python/client.py
+++
@@ -1,8 +0,0 @@
-import websocket
-
-ws = websocket.WebSocket()
-ws.connect('ws://192.168.2.204:45454')
-ws.send('{"amount":20,"endpoint":"PAYMENT","method":"CARD","order_tracking":"example_sale_from_test","receipt_email":"customer@gmail.com","receipt_sms":"0777777777","id":1}')
-while(True):
-    result = ws.recv()
-    print(result)
